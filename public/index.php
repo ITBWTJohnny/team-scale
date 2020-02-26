4d2ac687a2c6dbfe01366addb3d8b3b710ba09eb
IvanDeveloper2019 <ivan.levchenko@dott.pro>
2020-02-26 10:06:55 +0200

    deleted comments

--- public/index.php
+++ public/index.php
@@ -6,37 +6,6 @@ $rootDir = dirname($publicDir);
 define('__ROOT__', $rootDir);
 
 require_once($rootDir .'/vendor/autoload.php');
-//
-//$dsn = "mysql:host=localdev-mariadb;dbname=geonames";
-//$user = "root";
-//$passwd = "root";
-//
-//$pdo = new PDO($dsn, $user, $passwd);
-//$data = [];
-//
-//$stmt = $pdo->prepare('SELECT name, continent, phone from countryinfo');
-//
-//$stmt->execute();
-//
-//$data = $stmt->fetchAll();
-//$result = [];
-//
-//foreach ($data as $item) {
-////    dd($item);
-//    $phone = str_replace(['+', ' ', '-'], '', $item['phone']);
-//
-//    if (empty($phone)) continue;
-//
-//    $result[$phone] = $item['continent'];
-//}
-//
-//
-//file_put_contents(__ROOT__ .'/config/geoname.php', serialize($result));
-//
-//die();
-//
-//ob_start();
-
 
 require_once($rootDir . '/bootstrap/bootstrap.php');
 require_once($rootDir .'/config/routes.php');
